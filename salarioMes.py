a7b09e19fc8f7d83c355bf73e0c5ed8aa4127558
Rodrigo Rocha <rmaues.rocha@rmaues.com>
2019-04-22 13:43:06 -0300

    Ajustes no texto.

--- salarioMes.py
+++ salarioMes.py
@@ -1,7 +1,7 @@
-# Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+# Faça um Programa que calcule o vaor bruto de salário de uma pessoa com base na quantidade de horas e o valor da hora.
 __author__ = 'rmaues'
 
 vlrHora = float(input("Qual o valor da sua hora de trabalho? "))
-qntTrabalho = int(input("Quantas hotas trabalhou no mês ? "))
+qntTrabalho = int(input("Quantas horas trabalhou no mês ? "))
 
-print ("Seu salario este mês será de: ", qntTrabalho * vlrHora)
+print ("Seu salário este mês será de: ", qntTrabalho * vlrHora)
